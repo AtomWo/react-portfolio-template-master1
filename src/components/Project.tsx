6d8a6416872cf509f90ed3c669f0d5a8b872e337
AtomWo <100820361+AtomWo@users.noreply.github.com>
2025-09-06 00:13:46 +0000

    website

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -26,23 +26,23 @@ function Project() {
         <h1>Personal Projects</h1>
         <div className="projects-grid">
             <div className="project">
-                <a href="https://yujisatojr.itch.io/spacecraft" target="_blank" rel="noreferrer"><img src={cake} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://yujisatojr.itch.io/spacecraft" target="_blank" rel="noreferrer"><h2>Senior Design: Autonomous Vehicle </h2></a>
+                <a href="https://github.com/AtomWo/Senior-Design-Autonomous-Vehicle" target="_blank" rel="noreferrer"><img src={cake} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://github.com/AtomWo/Senior-Design-Autonomous-Vehicle" target="_blank" rel="noreferrer"><h2>Senior Design: Autonomous Vehicle </h2></a>
                 <p>Achieved 1st place fastest time from 20 teams in autonomous vehicle system using Python and OpenMV camera for real-time lane detection via blob detection/regions of interest(ROI), optimizing lane tracking through deflection angles.</p>
             </div>
             <div className="project">
-                <a href="https://www.filmate.club/" target="_blank" rel="noreferrer"><img src={ML} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://www.filmate.club/" target="_blank" rel="noreferrer"><h2>Pill Counter</h2></a>
+                <a href="https://drive.google.com/file/d/19ClP1Z4_KzG0uGXQO1QfjX6Q4ltXSxo6/view?usp=sharing" target="_blank" rel="noreferrer"><img src={ML} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://drive.google.com/file/d/19ClP1Z4_KzG0uGXQO1QfjX6Q4ltXSxo6/view?usp=sharing" target="_blank" rel="noreferrer"><h2>Pill Counter</h2></a>
                 <p>Developed a economical and efficient alternative to pill counting within Pharmaceuticals utilizing OpenCV and Python Object Oriented Programming.</p>
             </div>
             <div className="project">
-                <a href="https://yujisatojr.itch.io/highspeedchase" target="_blank" rel="noreferrer"><img src={Smart} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://yujisatojr.itch.io/highspeedchase" target="_blank" rel="noreferrer"><h2>Pan Lab: Smart-Probes</h2></a>
+                <a href="https://engineering.ucdavis.edu/news/smart-technology-zhongli-pan-insects-agriculture" target="_blank" rel="noreferrer"><img src={Smart} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://engineering.ucdavis.edu/news/smart-technology-zhongli-pan-insects-agriculture" target="_blank" rel="noreferrer"><h2>Pan Lab: Smart-Probes</h2></a>
                 <p>Developed and debugged Nordic semiconductor modeled “SmartProbes” using C/C++ assembly and Zephyr’s interprocess communication mechanisms, utilizing eSPI IDE and ESP-32 for input output communication and memory allocation.</p>
             </div>
             <div className="project">
-                <a href="https://yujisatojr.itch.io/spacecraft" target="_blank" rel="noreferrer"><img src={CAN} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://yujisatojr.itch.io/spacecraft" target="_blank" rel="noreferrer"><h2>CAN Breakout Board (Formula Racing) </h2></a>
+                <a href="https://fsae.ucdavis.edu/" target="_blank" rel="noreferrer"><img src={CAN} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://fsae.ucdavis.edu/" target="_blank" rel="noreferrer"><h2>CAN Breakout Board (Formula Racing) </h2></a>
                 <p>Utilizing Altium Designer for the block diagram, schematic capture, and layout design of the Formula SAE racecar’s PCBs enabling real-time communication between ECUs for CAN protocol and loopback testing logic.</p>
             </div>
             <div className="project">
